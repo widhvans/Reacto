c8e1c8bdbb951545fee4f491823f14ef5f02718a
widhvans <110589162+widhvans@users.noreply.github.com>
2025-12-17 17:41:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "123456"))  # Replace with your API ID
-    API_HASH = os.getenv("API_HASH", "your_api_hash") # Replace with your API Hash
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token") # Replace with your Bot Token
-    MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://...") # Your MongoDB Connection String
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789")) # Your personal Telegram ID
+    API_ID = int(os.getenv("API_ID", "32618962"))  # Replace with your API ID
+    API_HASH = os.getenv("API_HASH", "e30edea9a94912fad51753d80c7299bc") # Replace with your API Hash
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7759947933:AAFKx1H_Tn39mvg_6lMRAbd3Ox0IQSRxgGY") # Replace with your Bot Token
+    MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Your MongoDB Connection String
+    OWNER_ID = int(os.getenv("OWNER_ID", "8110137558")) # Your personal Telegram ID
   
